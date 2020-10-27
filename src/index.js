8e77ae1f9bd04b9eb5a22b03446b8b913f6c96f0
viskipaul <viskipaul@gmail.com>
2020-10-27 17:28:34 +0200

    Complete car rest service

--- src/index.js
+++ src/index.js
@@ -25,21 +25,20 @@ app.use(async (ctx, next) => {
   }
 });
 
-class Item {
-  constructor({ id, text, date, version }) {
+class Car{
+  constructor({id, model, year}) {
     this.id = id;
-    this.text = text;
-    this.date = date;
-    this.version = version;
+    this.model = model;
+    this.year = year;
   }
 }
 
-const items = [];
-for (let i = 0; i < 3; i++) {
-  items.push(new Item({ id: `${i}`, text: `item ${i}`, date: new Date(Date.now() + i), version: 1 }));
-}
-let lastUpdated = items[items.length - 1].date;
-let lastId = items[items.length - 1].id;
+const cars = []
+cars.push(new Car({ id: '1', model: 'Dacia Logan', year: '2006'}));
+cars.push(new Car({ id: '2', model: 'Volkswagen Golf', year: '2009'}));
+
+// let lastUpdated = cars[cars.length - 1].date;
+let lastId = 2
 const pageSize = 10;
 
 const broadcast = data =>
@@ -51,115 +50,94 @@ const broadcast = data =>
 
 const router = new Router();
 
-router.get('/item', ctx => {
+router.get('/car', ctx => {
   const ifModifiedSince = ctx.request.get('If-Modif ied-Since');
-  if (ifModifiedSince && new Date(ifModifiedSince).getTime() >= lastUpdated.getTime() - lastUpdated.getMilliseconds()) {
-    ctx.response.status = 304; // NOT MODIFIED
-    return;
-  }
+
   const text = ctx.request.query.text;
   const page = parseInt(ctx.request.query.page) || 1;
-  ctx.response.set('Last-Modified', lastUpdated.toUTCString());
-  const sortedItems = items
-    .filter(item => text ? item.text.indexOf(text) !== -1 : true)
-    .sort((n1, n2) => -(n1.date.getTime() - n2.date.getTime()));
-  const offset = (page - 1) * pageSize;
-  // ctx.response.body = {
-  //   page,
-  //   items: sortedItems.slice(offset, offset + pageSize),
-  //   more: offset + pageSize < sortedItems.length
-  // };
-  ctx.response.body = items;
+  ctx.response.body = cars;
   ctx.response.status = 200;
 });
 
-router.get('/item/:id', async (ctx) => {
-  const itemId = ctx.request.params.id;
-  const item = items.find(item => itemId === item.id);
-  if (item) {
-    ctx.response.body = item;
+router.get('/car/:id', async (ctx) => {
+  const carId = ctx.params.id;
+  const car = cars.find(car => carId === car.id);
+  if (car) {
+    ctx.response.body = car;
     ctx.response.status = 200; // ok
   } else {
-    ctx.response.body = { issue: [{ warning: `item with id ${itemId} not found` }] };
+    ctx.response.body = { issue: [{ warning: `item with id ${carId} not found` }] };
     ctx.response.status = 404; // NOT FOUND (if you know the resource was deleted, then return 410 GONE)
   }
 });
 
 const createItem = async (ctx) => {
-  const item = ctx.request.body;
-  if (!item.text) { // validation
-    ctx.response.body = { issue: [{ error: 'Text is missing' }] };
-    ctx.response.status = 400; //  BAD REQUEST
-    return;
+  const car = ctx.request.body;
+  if(!car.model){
+    ctx.response.body = { issue: [{ error: 'Model is missing' }] };
+    ctx.response.status = 400;
   }
-  item.id = `${parseInt(lastId) + 1}`;
-  lastId = item.id;
-  item.date = Date.now();
-  item.version = 1;
-  items.push(item);
-  ctx.response.body = item;
+  if(!car.year){
+    ctx.response.body = { issue: [{ error: 'Year is missing' }] };
+    ctx.response.status = 400;
+  }
+  car.id = `${parseInt(lastId) + 1}`;
+  lastId = car.id;
+  cars.push(car);
+  ctx.response.body = car;
   ctx.response.status = 201; // CREATED
-  broadcast({ event: 'created', payload: { item } });
+  broadcast({ event: 'created', payload: { car } });
 };
 
-router.post('/item', async (ctx) => {
+router.post('/car', async (ctx) => {
   await createItem(ctx);
 });
 
-router.put('/item/:id', async (ctx) => {
+router.put('/car/:id', async (ctx) => {
   const id = ctx.params.id;
-  const item = ctx.request.body;
-  const itemId = item.id;
-  if (itemId && id !== item.id) {
+  const car = ctx.request.body;
+  const carId = car.id;
+  if (carId && id !== car.id) {
     ctx.response.body = { issue: [{ error: `Param id and body id should be the same` }] };
     ctx.response.status = 400; // BAD REQUEST
     return;
   }
-  if (!itemId) {
+  if (!carId) {
     await createItem(ctx);
     return;
   }
-  const index = items.findIndex(item => item.id === id);
+  const index = cars.findIndex(car => car.id === id);
   if (index === -1) {
     ctx.response.body = { issue: [{ error: `item with id ${id} not found` }] };
     ctx.response.status = 400; // BAD REQUEST
     return;
   }
-  const itemVersion = parseInt(ctx.request.get('ETag')) || item.version;
-  if (itemVersion < items[index].version) {
-    ctx.response.body = { issue: [{ error: `Version conflict` }] };
-    ctx.response.status = 409; // CONFLICT
-    return;
-  }
-  item.version++;
-  items[index] = item;
-  lastUpdated = new Date();
-  ctx.response.body = item;
+  cars[index] = car;
+  ctx.response.body = car;
   ctx.response.status = 200; // OK
-  broadcast({ event: 'updated', payload: { item } });
+  broadcast({ event: 'updated', payload: { car } });
 });
 
-router.del('/item/:id', ctx => {
+router.del('/car/:id', ctx => {
   const id = ctx.params.id;
-  const index = items.findIndex(item => id === item.id);
+  const index = items.findIndex(car => id === car.id);
   if (index !== -1) {
-    const item = items[index];
-    items.splice(index, 1);
-    lastUpdated = new Date();
+    const car = cars[index];
+    cars.splice(index, 1);
     broadcast({ event: 'deleted', payload: { item } });
   }
   ctx.response.status = 204; // no content
 });
 
-setInterval(() => {
-  lastUpdated = new Date();
-  lastId = `${parseInt(lastId) + 1}`;
-  const item = new Item({ id: lastId, text: `item ${lastId}`, date: lastUpdated, version: 1 });
-  items.push(item);
-  console.log(`
-   ${item.text}`);
-  broadcast({ event: 'created', payload: { item } });
-}, 15000);
+// setInterval(() => {
+//   lastUpdated = new Date();
+//   lastId = `${parseInt(lastId) + 1}`;
+//   const item = new Item({ id: lastId, text: `item ${lastId}`, date: lastUpdated, version: 1 });
+//   items.push(item);
+//   console.log(`
+//    ${item.text}`);
+//   broadcast({ event: 'created', payload: { item } });
+// }, 15000);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
